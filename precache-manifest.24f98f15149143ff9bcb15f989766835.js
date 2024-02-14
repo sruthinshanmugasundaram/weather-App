9282976928695a6243367f01dc38b6b8ce37e2f8
sruthinshanmugasundaram <sruthinsruthin70@gmail.com>
2024-02-15 00:11:07 +0530

    Updates

---
+++ precache-manifest.24f98f15149143ff9bcb15f989766835.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17a930fccb594626b7fd2320ac47dfb",
+    "url": "/netflix-project/index.html"
+  },
+  {
+    "revision": "a984c18f22c7e28a6e7b",
+    "url": "/netflix-project/static/css/main.488734f7.chunk.css"
+  },
+  {
+    "revision": "8331df34dcc0368023eb",
+    "url": "/netflix-project/static/js/2.f35d591e.chunk.js"
+  },
+  {
+    "revision": "a984c18f22c7e28a6e7b",
+    "url": "/netflix-project/static/js/main.e1fac525.chunk.js"
+  },
+  {
+    "revision": "f886a21cb9b4d5112452",
+    "url": "/netflix-project/static/js/runtime~main.f4b0cc94.js"
+  }
+]);
